f6c636bddd1147c9eef8fbc67dfbdcd27b814904
jlleivas <jlleivas@L0000661.vectoritcgroup.com>
2018-06-11 14:59:08 -0400

    (VStago) Atributos actualizados para la sincronizacion, falta leer lastUpdate global desde xml y generar algunas funciones de inicio

--- src/fn/GlobalValues.java
+++ src/fn/GlobalValues.java
@@ -5,8 +5,15 @@
  */
 package fn;
 
-import entities.Ficha;
+import entities.Cliente;
+import entities.Cristal;
+import entities.Descuento;
+import entities.Doctor;
+import entities.Institucion;
 import entities.Lente;
+import entities.Oficina;
+import entities.RegistroBaja;
+import entities.TipoPago;
 import entities.User;
 import fn.close.Close;
 import static fn.xml.ReadXml.readLocalXml;
@@ -14,8 +21,19 @@ import static fn.xml.ReadXml.readLocalLastUpdateXml;
 import static fn.xml.ReadXml.readRemoteLastUpdateXmlAndCreateValidateXml;
 import fn.xml.SaveXml;
 import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import javax.swing.JOptionPane;
+import newpackage.NoGit;
+import sync.Cmp;
+import sync.entities.Global;
+import sync.entities.Local;
+import sync.entities.Remote;
 import view.OPanel;
 
 /**
@@ -28,10 +46,10 @@ public class GlobalValues {
     public static String VERSION = "4.0.0";
     
     /* Bases de datos*/
-    public static String BD_URL_REMOTE = "localhost";
-    public static String BD_NAME_REMOTE = "localhost/odmbd";
-    public static String BD_USER_REMOTE = "root";
-    public static String BD_PASS_REMOTE = "";
+    public static String BD_URL_REMOTE = NoGit.URL;
+    public static String BD_NAME_REMOTE = NoGit.DB;
+    public static String BD_USER_REMOTE = NoGit.USER;
+    public static String BD_PASS_REMOTE = NoGit.PASS;
     public static String BD_URL_LOCAL = "localhost:1527";
     public static String BD_NAME_LOCAL = "odm4";
     public static String BD_USER_LOCAL = "odm4";
@@ -56,6 +74,7 @@ public class GlobalValues {
     public static String MAIL_ADDRES = "sdx.respaldo.bd@gmail.com";
     public static String MAIL_PASS= "qwpzedzqucvpyjzt";
     public static String MAIL_REPORT= "softdirex@gmail.com";
+    public static String MAIL_LOG = "";
     
     /* Direcciones de fichero*/
     public static String FILES_PATH = "";
@@ -69,11 +88,26 @@ public class GlobalValues {
     public static int TMP_ID_DESCUENTO;
     public static Date TMP_DATE_FROM = null;
     public static Date TMP_DATE_TO =null;
-    //LISTAS TEMPORALES
-    public static ArrayList<Ficha> TMP_LIST_FICHAS = new ArrayList<Ficha>();
-    public static ArrayList<Lente> TMP_LIST_LENTES = new ArrayList<Lente>();
-    public static ArrayList<User> TMP_LIST_USERS = new ArrayList<User>();
+    /*  Sincronizacion */
+    public static Global GLOBAL_SYNC = new Global();
+    public static Local LOCAL_SYNC = new Local();
+    public static Remote REMOTE_SYNC = new Remote();
+    public static boolean IS_ONLINE = false;
+    public static Date LAST_UPDATE;
     
+    //LISTAS TEMPORALES
+    public static ArrayList<User> TMP_LIST_USERS = new ArrayList<User>();
+    public static ArrayList<Cristal> TMP_LIST_CRISTAL = new ArrayList<Cristal>();
+    public static ArrayList<Descuento> TMP_LIST_DESCUENTO = new ArrayList<Descuento>();
+    public static ArrayList<Cliente> TMP_LIST_CLIENTES = new ArrayList<Cliente>();
+    public static ArrayList<Doctor> TMP_LIST_DOCTORES = new ArrayList<Doctor>();
+    public static ArrayList<Institucion> TMP_LIST_INSTITUCIONES = new ArrayList<Institucion>();
+    public static ArrayList<Lente> TMP_LIST_LENTES = new ArrayList<Lente>();
+    public static ArrayList<Oficina> TMP_LIST_OFICINAS = new ArrayList<Oficina>();
+    public static ArrayList<RegistroBaja> TMP_LIST_REGISTROS_BAJAS = new ArrayList<RegistroBaja>();
+    public static ArrayList<TipoPago> TMP_LIST_TIPOS_PAGO = new ArrayList<TipoPago>();
+    
+    /* Joption Pane del sistema */
     /* Joption Pane del sistema */
     public static OPanel INFOPANEL = new OPanel();
     public static String PANELTITLE ="";
@@ -89,6 +123,8 @@ public class GlobalValues {
         System.out.println("GLOBALVALUES:initValues()");
         LOCAL_PATH = System.getProperty("user.dir")+File.separator;
         FILES_PATH = LOCAL_PATH+"files"+File.separator;
+        SubProcess.isOnline();
+        loadLastUpdateFromXML();//cargar LAST_UPDATE de fichero xml al iniciar programa
         try{
             setProjectName();
             readLocalXml();
@@ -125,7 +161,6 @@ public class GlobalValues {
         int pos = System.getProperty("user.dir").lastIndexOf(File.separator);
         PROJECTNAME = System.getProperty("user.dir").substring(pos+1);
     }
-
     public static String getLocalBdUser() {
         return BD_USER_LOCAL;
     }
@@ -166,5 +201,67 @@ public class GlobalValues {
         return MAIL_PASS;
     }
     
+    
+    public static boolean isOnline(){
+        return IS_ONLINE;
+    }
+
+    public static String getEqId() throws UnknownHostException{
+        return thisPcAddress()+"/"+thisPcName();
+    }
+    
+    private static String thisPcAddress() throws UnknownHostException{
+        InetAddress localHost = InetAddress.getLocalHost();
+        return localHost.getHostAddress();
+    }
+    
+    private static String thisPcName() throws UnknownHostException{
+        InetAddress localHost = InetAddress.getLocalHost();
+        return localHost.getHostName();
+    }
+    private static void loadLastUpdateFromXML() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Obtiene el numero del equipo desde la licencia actual para insertar en base de datos
+     * Ejemplo: "1-7000" donde 1 es el número del equipo y 7000 es el id correlativo
+     * @return inicio del Id para asignarlo a la respectiva clase
+     */
+    public static String getIndexId(){
+        try{
+            int index = Integer.parseInt(LOCAL_ID.substring((LOCAL_ID.length())-1));
+            return index+"-";
+        }catch(NumberFormatException ex){
+            OptionPane.showMsg("Error Fatal", "Se ha detectado un error fatal en su sistema,"
+                    + "\nsi continúa insertando registros puede dañar todos los datos de su empresa."
+                    + "\nLe recomendamos contactarse con su proveedor de software inmediatamente."
+                    + "\nPuede estar siendo víctima de una adulteración de licencia.", JOptionPane.ERROR_MESSAGE);
+        }
+        return "Error-";
+    }
+    
+    public static String getPublicIp() throws IOException{
+        return Cmp.getPublicIp();
+    }
+    
+    public static int contChar(char toCompare, String toCount){
+        char[] arrayChar = toCount.toCharArray();
+        int cont=0;
+        for(int i=0; i<arrayChar.length; i++){
+
+                if( arrayChar[i] == toCompare){
+                    cont++;
+                }
+        }
+        return cont;
+    }
+    
+    public static int hourToInt(Date date){
+        DateFormat hourFormat = new SimpleDateFormat("HHmmss");
+        String formatHour = hourFormat.format(date);
+        int hora = Integer.parseInt(formatHour);
+        return hora;
+    }
 
 }
