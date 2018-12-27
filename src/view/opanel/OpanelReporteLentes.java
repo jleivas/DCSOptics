f45d09a8f530237b46496f8f77d6c75a6c6b8c2f
Jorge Leiva <softdirex@gmail.com>
2018-12-27 14:21:29 -0300

    (v-stgoDELL)[V412-004]Validación de conexión a internet al enviar reporte por email

--- src/view/opanel/OpanelReporteLentes.java
+++ src/view/opanel/OpanelReporteLentes.java
@@ -34,27 +34,27 @@ public class OpanelReporteLentes extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnCancelar = new javax.swing.JLabel();
+        btnSendReport = new javax.swing.JLabel();
         btnGuardar = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         txtReporte = new javax.swing.JTextArea();
 
         setBackground(new java.awt.Color(255, 255, 255));
 
-        btnCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/icons8_Forward_Message_50px.png"))); // NOI18N
-        btnCancelar.setToolTipText("Enviar reporte");
-        btnCancelar.addMouseListener(new java.awt.event.MouseAdapter() {
+        btnSendReport.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/icons8_Forward_Message_50px.png"))); // NOI18N
+        btnSendReport.setToolTipText("Enviar reporte");
+        btnSendReport.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnCancelarMouseClicked(evt);
+                btnSendReportMouseClicked(evt);
             }
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                btnCancelarMouseEntered(evt);
+                btnSendReportMouseEntered(evt);
             }
             public void mouseExited(java.awt.event.MouseEvent evt) {
-                btnCancelarMouseExited(evt);
+                btnSendReportMouseExited(evt);
             }
             public void mousePressed(java.awt.event.MouseEvent evt) {
-                btnCancelarMousePressed(evt);
+                btnSendReportMousePressed(evt);
             }
         });
 
@@ -87,7 +87,7 @@ public class OpanelReporteLentes extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 497, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnCancelar)
+                        .addComponent(btnSendReport)
                         .addGap(18, 18, 18)
                         .addComponent(btnGuardar)))
                 .addContainerGap(20, Short.MAX_VALUE))
@@ -99,27 +99,31 @@ public class OpanelReporteLentes extends javax.swing.JPanel {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnCancelar)
+                    .addComponent(btnSendReport)
                     .addComponent(btnGuardar))
                 .addGap(21, 21, 21))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnCancelarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCancelarMouseClicked
-        OptionPane.showOptionPanel(new OpanelSelectAdminToSendReporteInventario(), "Enviar reporte de inventario");
-    }//GEN-LAST:event_btnCancelarMouseClicked
+    private void btnSendReportMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSendReportMouseClicked
+        if(GV.isOnline()){
+            OptionPane.showOptionPanel(new OpanelSelectAdminToSendReporteInventario(), "Enviar reporte de inventario");
+        }else{
+            OptionPane.showMsg("No se puede enviar reporte", "El sistema no ha detectado ninguna conexión a internet",2);
+        }
+    }//GEN-LAST:event_btnSendReportMouseClicked
 
-    private void btnCancelarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCancelarMouseEntered
-        btnCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource(Icons.getEnteredIcon(btnCancelar.getIcon().toString()))));
-    }//GEN-LAST:event_btnCancelarMouseEntered
+    private void btnSendReportMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSendReportMouseEntered
+        btnSendReport.setIcon(new javax.swing.ImageIcon(getClass().getResource(Icons.getEnteredIcon(btnSendReport.getIcon().toString()))));
+    }//GEN-LAST:event_btnSendReportMouseEntered
 
-    private void btnCancelarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCancelarMouseExited
-        btnCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource(Icons.getExitedIcon(btnCancelar.getIcon().toString()))));
-    }//GEN-LAST:event_btnCancelarMouseExited
+    private void btnSendReportMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSendReportMouseExited
+        btnSendReport.setIcon(new javax.swing.ImageIcon(getClass().getResource(Icons.getExitedIcon(btnSendReport.getIcon().toString()))));
+    }//GEN-LAST:event_btnSendReportMouseExited
 
-    private void btnCancelarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCancelarMousePressed
+    private void btnSendReportMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSendReportMousePressed
         // TODO add your handling code here:
-    }//GEN-LAST:event_btnCancelarMousePressed
+    }//GEN-LAST:event_btnSendReportMousePressed
 
     private void btnGuardarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnGuardarMouseClicked
         OptionPane.closeOptionPanel();
@@ -139,8 +143,8 @@ public class OpanelReporteLentes extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel btnCancelar;
     private javax.swing.JLabel btnGuardar;
+    private javax.swing.JLabel btnSendReport;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea txtReporte;
     // End of variables declaration//GEN-END:variables
